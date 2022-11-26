a3890c62033345b6332ad1c68343f2422c8e85f7
manojmanikam <manojkrishnasamy2002@gmail.com>
2022-11-26 10:21:44 +0530

    hey

---
+++ src/org/demo/Details.java
@@ -0,0 +1,21 @@
+package org.demo;
+
+public class Details {
+
+	private void id() {
+		System.out.println("311908061");
+
+	}
+	
+	private void address() {
+		System.out.println("Tiruvottiyur");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Details a = new Details();
+		a.address();
+		a.id();
+	}
+}
